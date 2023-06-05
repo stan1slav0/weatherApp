bc8932b446a76556dfa82aec050a63b49f968bce
Stanislav <pingo.mw2@gmail.com>
2023-06-05 18:07:33 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from 'axios'
+
 
 function App() {
+
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+  const [errorMsg, setErrorMsg] = useState('')
+
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=49a2ad8bc7b7c860d1da5b861a8f7ce7&units=metric`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(URL)
+        .then(res => {
+          setData(res.data);
+          console.log(res.data);
+          setErrorMsg('')
+        })
+        .catch(error => {
+            setErrorMsg('City not found')
+            setData(null)
+        });
+        
+      setLocation('');
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyDown={searchLocation}
+          placeholder="Enter location"
+          type="text"
+        />
+      </div>
+      {errorMsg && <h3 className="error">{errorMsg}</h3>}
+
+      {data && (
+        <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}{data.sys?.country && `, ${data.sys.country}`}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{Math.round(data.main.temp)}  &#8451;</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+       {data.name &&  
+       <div className="bottom">
+          <div className="feels">
+            {data.main ? <p className="bold">{Math.round(data.main.feels_like)}  &#8451;</p> : null}
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            {data.main ? <p className="bold">{data.main.humidity} %</p> : null}
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            {data.wind ? <p className="bold">{data.wind.speed}  m/s</p> : null}
+            <p>Wind</p>
+          </div>
+        </div>}
+      </div>
+      )}
     </div>
   );
 }
